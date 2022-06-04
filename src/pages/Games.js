ba48af1bea3bc902cfe8b5a126eac5e35f1258ec
Alex Shaham <ajshaham@gmail.com>
2022-06-04 12:58:42 -0700

    Converted the fetch to an import, and now site properly loads the data.

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -2,23 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Grid, Container, Toolbar } from "@mui/material"
 import ProjectCard from '../components/ProjectCard';
 
+import data from '../data/db.json';
+
 const Games = () => {
-  const [games, setGames] = useState([])
-
-  console.log('part 1: ', Object.keys(games));
-  useEffect(() => {
-    fetch('/data/db.json')
-      .then(response => response.json())
-      .then(({ games }) => games)
-      .then(data => setGames(data))
-  }, []);
-  console.log('part 2: ', Object.keys(games));
-
   return (
     <Container>
       <Toolbar sx={{minHeight: '20px !important'}}/>
       <Grid container spacing={3} sx={{ flexGrow: 1 }}>
-        {games.map(game => (
+        {data.games.map(game => (
           <Grid item key={game.id} xs={12} md={6} lg={4}>
             <ProjectCard project={game}/>
           </Grid>
