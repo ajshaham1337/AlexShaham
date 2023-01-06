e611fe382d3b27967da05dfcf39a6439b2d30ad3
Alex Shaham <ajshaham@gmail.com>
2023-01-05 20:28:55 -0800

    moved all files to the root folder as opposed to a 'client' folder

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // experimental: {
+  //   webVitalsAttribution: ['CLS', 'FID', 'FCP', 'INP', 'LCP', 'TTFB']
+  // }
+}
+
+module.exports = nextConfig
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   images: {
+//     loader: "akamai",
+//     path: "",
+//   },
+//   assetPrefix: "./",
+// };
+
+// export default nextConfig;
